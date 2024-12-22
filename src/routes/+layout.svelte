9b08a974b8db9ab3849f4f9a7a2b6fc3bab16114
bmehder <bmehder@gmail.com>
2024-12-22 18:00:53 +0000

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import "$lib/style.css";
+
+  let { children } = $props();
+</script>
+
+<div id="wrapper">
+  <main>
+    {@render children()}
+  </main>
+</div>
+
+<style>
+  #wrapper {
+    background-image: url("/pattern.png");
+  }
+
+  main {
+    min-height: 100dvh;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,64 @@
+<svelte:head>
+  <title
+    >Allstate Background Searches - Southern California | Allstate Background
+    Searches</title
+  >
+  <meta
+    name="description"
+    content="Allstate Background Searches specializes in pre-employment background screening and tenant background screening in Southern California. Get started today!"
+  />
+</svelte:head>
+
+<section>
+  <div class="outer">
+    <div class="inner flow" style="--lg: var(--sm)">
+      <h1>
+        <span class="visually-hidden">
+          Allstate Backgrounds Searches, LLC
+        </span>
+        <img
+          class="logo"
+          src="/logo-allstate.webp"
+          alt="Allstate Backgrounds logo"
+        />
+      </h1>
+      <div class="tagline h3">
+        We specialize in pre-employment background screening and tenant
+        background screening in Southern California.
+      </div>
+      <div class="description">
+        Whether you are an employer hiring a new worker or a landlord looking to
+        rent, Allstate Background Searches, LLC makes sure you are getting
+        honest information from your applicants.
+      </div>
+      <div class="contact-info">
+        <p><strong> Call Us:</strong> (916) 341-9913</p>
+        <p>
+          <strong>Email Us: </strong>
+          <a href="mailto:service@allstatebackgrounds.com" target="_blank"
+            >service@allstatebackgrounds.com</a
+          >
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+  .inner {
+    min-height: 100dvh;
+    display: grid;
+    align-content: center;
+    /* justify-items: center; */
+  }
+
+  .logo {
+    max-width: var(--xs);
+    margin-inline: auto;
+  }
+
+  .tagline {
+    line-height: inherit;
+    text-wrap: initial;
+  }
+</style>
