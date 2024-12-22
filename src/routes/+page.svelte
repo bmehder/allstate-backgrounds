0f400c1324eed6dac56b81be56c468ad03624d43
bmehder <bmehder@gmail.com>
2024-12-22 18:42:45 +0000

    styling updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,34 +11,39 @@
 
 <section>
   <div class="outer">
-    <div class="inner flow" style="--lg: var(--sm)">
-      <h1>
-        <span class="visually-hidden">
-          Allstate Backgrounds Searches, LLC
-        </span>
-        <img
-          class="logo"
-          src="/logo-allstate.webp"
-          alt="Allstate Backgrounds logo"
-        />
-      </h1>
-      <div class="tagline h3">
-        We specialize in pre-employment background screening and tenant
-        background screening in Southern California.
-      </div>
-      <div class="description">
-        Whether you are an employer hiring a new worker or a landlord looking to
-        rent, Allstate Background Searches, LLC makes sure you are getting
-        honest information from your applicants.
-      </div>
-      <div class="contact-info">
-        <p><strong> Call Us:</strong> (916) 341-9913</p>
-        <p>
-          <strong>Email Us: </strong>
-          <a href="mailto:service@allstatebackgrounds.com" target="_blank"
-            >service@allstatebackgrounds.com</a
-          >
-        </p>
+    <div class="inner" style="--lg: var(--sm)">
+      <div class="content flow">
+        <h1>
+          <span class="visually-hidden">
+            Allstate Backgrounds Searches, LLC
+          </span>
+          <img
+            class="logo"
+            src="/logo-allstate.webp"
+            alt="Allstate Backgrounds logo"
+          />
+        </h1>
+        <div class="tagline h3">
+          We specialize in pre-employment background screening and tenant
+          background screening in Southern California.
+        </div>
+        <div class="description">
+          Whether you are an employer hiring a new worker or a landlord looking
+          to rent, Allstate Background Searches, LLC makes sure you are getting
+          honest information from your applicants.
+        </div>
+        <div class="contact-info">
+          <p>
+            <strong> Call Us:</strong>
+            <a href="tel:9163419913">(916) 341-9913</a>
+          </p>
+          <p>
+            <strong>Email Us: </strong>
+            <a href="mailto:service@allstatebackgrounds.com" target="_blank"
+              >service@allstatebackgrounds.com</a
+            >
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -48,8 +53,20 @@
   .inner {
     min-height: 100dvh;
     display: grid;
-    align-content: center;
-    /* justify-items: center; */
+    place-content: center;
+  }
+
+  .content {
+    display: grid;
+    place-content: center;
+    padding: var(--size-3);
+    background-color: white;
+    border-radius: var(--size-2);
+    box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.2);
+  }
+
+  .description {
+    color: #595959;
   }
 
   .logo {
